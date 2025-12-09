6aef463b706b5c926af67d0e9780f2bb0efec445
Álvaro Colmenero <acolrod814@g.educaand.es>
2025-12-09 16:44:13 +0100

    Mas cambios

--- funciones/funcionesAlvaro.py
+++ funciones/funcionesAlvaro.py
@@ -1,7 +1,18 @@
-def Equipos(datosliga) -> set:
+def equipos(datosliga) -> set:
+
+    """
+    Recibe la lista de diccionarios generado a partir de la 
+    función anterior, genera los datos de la clasificación y los imprime por pantalla.
+    
+    :param datosliga: Le pasamos los datos extraidos del csv
+    :return: Retorna los equipos
+    :rtype: set
+    """
+
     equipos = set()
     for partido in datosliga:
         equipos.add(partido["Team 1"])
         equipos.add(partido["Team 2"])
     return equipos
+
 
