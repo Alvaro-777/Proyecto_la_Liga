e48ee075ca4d3854cbbd0a66802facc38f12b6dc
Alejandro Medel Martínez <amm00331@red.ujaen.es>
2025-12-09 16:38:59 +0100

    Docstring de la funcion leerPartidos()

--- funciones/Todas_las_funciones.py
+++ funciones/Todas_las_funciones.py
@@ -1,6 +1,13 @@
 import csv
 
 def leerPartidos() -> list:
+    """
+    Función que lee el archivo liga.csv y lo devuelve como
+    una lista de diccionarios
+    
+    :return: El contenido del archivo en una lista de diccionarios
+    :rtype: list[dict]
+    """
     with open('liga.csv', 'r', encoding="utf-8",) as fCSV:
         content = list(csv.DictReader(fCSV))
     fCSV.close()
--- funciones/funcionesAlex.py
+++ funciones/funcionesAlex.py
@@ -1,7 +1,15 @@
-import csv
+import csv, funcionesAlvaro as fAvl
 
-def leerPartidos() -> list:
+def leerPartidos() -> list[dict]:
+    """
+    Función que lee el archivo liga.csv y lo devuelve como
+    una lista de diccionarios
+    
+    :return: El contenido del archivo en una lista de diccionarios
+    :rtype: list[dict]
+    """
     with open('liga.csv', 'r', encoding="utf-8",) as fCSV:
         content = list(csv.DictReader(fCSV))
     fCSV.close()
     return content
+
