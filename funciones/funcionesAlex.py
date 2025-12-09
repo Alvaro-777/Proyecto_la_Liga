c9f708334bc073ef8297aeee16c8a49716075907
Alejandro Medel Martínez <amm00331@red.ujaen.es>
2025-12-09 16:16:35 +0100

    Añadida la funcion leerPartidos()

--- funciones/funcionesAlex.py
+++ funciones/funcionesAlex.py
@@ -1,5 +1,7 @@
+import csv
+
 def leerPartidos() -> list:
-    with open('../') as fCSV:
-        fCSV.read()
+    with open('liga.csv', 'r', encoding="utf-8",) as fCSV:
+        content = list(csv.reader(fCSV))
     fCSV.close()
-    return []
+    return content
