00e8efe18a005d91ba612c3620f880de06b5383f
Álvaro Colmenero <acolrod814@g.educaand.es>
2025-12-09 16:15:35 +0100

    Subida_a_ver_si_funciona_esto

---
+++ funciones/funcionesAlvaro.py
@@ -0,0 +1,11 @@
+def suma(a, b):
+    return a + b
+
+print("¡Prueba de Python funcionando!")
+
+num1 = int(input("Ingresa un número: "))
+num2 = int(input("Ingresa otro número: "))
+
+resultado = suma(num1, num2)
+
+print(f"La suma de {num1} y {num2} es: {resultado}")
