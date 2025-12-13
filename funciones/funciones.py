7453731383f3a1c67934c83b54c6a36c2504781f
Alejandro Medel Martínez <amm00331@red.ujaen.es>
2025-12-13 18:49:50 +0100

    Añadí las funciones quienGana() y clasificacion(), y incluí clasificación() en el funcionamiento de impClasificacion()

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -1,6 +1,6 @@
 import csv
 
-def leerPartidos() -> list:
+def leerPartidos() -> list[dict]:
     """
     Función que lee el archivo liga.csv y lo devuelve como
     una lista de diccionarios
@@ -13,9 +13,25 @@ def leerPartidos() -> list:
     fCSV.close()
     return content
 
-def impClasificacionLiga(liga) -> None:
-    equipos(liga)
+def impClasificacion(liga: list[dict]) -> None:
+    """
+    Genera la clasificación y la muestra por pantalla
+    
+    :param liga: Datos de la liga
+    """
+    conj_equipos = equipos(liga)
+    datos = infoEquipos(liga, conj_equipos)
 
+    # ordenar por puntos (último elemento de la tupla)
+    datos = clasificacion(datos)
+
+    print(" Nº|     Equipo      | PG | PE | PP | Puntos")
+    print("---------------------------------------------")
+
+    posicion = 1
+    for equipo, pg, pe, pp, pts in datos:
+        print(f"{posicion:2} | {equipo:15} | {pg:2} | {pe:2} | {pp:2} | {pts:3}")
+        posicion += 1
 
 def equipos(datosliga) -> set:
     """
@@ -32,30 +48,13 @@ def equipos(datosliga) -> set:
         equipos.add(partido["Team 2"])
     return equipos
 
-def infoEquipos (datosLiga: list,equipos: set) -> list[tuple]:
-    return list() 
-
-def quienGana(resultado) -> int:
-    return 0
-
-def puntos(info: list) -> int:
-    return 0
-
-def clasificacion(datos: list) -> list:
-    return list() 
-
-#---------------------------------------------
-
-datosliga = leerPartidos()
-impClasificacionLiga(datosliga)
-
-def infoEquipos(datosLiga: list, equipos: set) -> list[tuple]:
+def infoEquipos(datosLiga: list[dict], equipos: set) -> list[tuple]:
     """
     Recibe los datos de la liga y el conjunto de equipos.
     Devuelve una lista de tuplas:
     (equipo, ganados, empatados, perdidos, puntos)
     """
-    info = []
+    info = list[tuple]()
 
     for equipo in equipos:
         ganados = 0
@@ -86,31 +85,38 @@ def infoEquipos(datosLiga: list, equipos: set) -> list[tuple]:
 
     return info
 
+def quienGana(resultado: str) -> int:
+    separatorIndex = resultado.index('-')
+    ptsCasa = int(resultado[:separatorIndex])
+    ptsVist = int(resultado[separatorIndex:])
+    
+    if ptsCasa > ptsVist: 
+        return 1
+    if ptsCasa < ptsVist:
+        return -1
+    return 0
 
 def puntos(info: list) -> int:
     """
     Recibe una lista [ganados, empatados, perdidos]
     y devuelve los puntos (3-1-0)
+    
+    :param info: Descripción
+    :type info: list
+    :return: Descripción
+    :rtype: int
     """
     ganados = info[0]
     empatados = info[1]
     return ganados * 3 + empatados
 
+def clasificacion(datos: list[tuple]) -> list:
+    return sorted(datos, key=lambda x: x[4], reverse=True) 
 
-def impClasificacion(liga) -> None:
-    """
-    Genera la clasificación y la muestra por pantalla
-    """
-    conj_equipos = equipos(liga)
-    datos = infoEquipos(liga, conj_equipos)
+#---------------------------------------------
 
-    # ordenar por puntos (último elemento de la tupla)
-    datos_ordenados = sorted(datos, key=lambda x: x[4], reverse=True)
+datosliga = leerPartidos()
+#print(datosliga[0]['FT'])
+impClasificacion(datosliga)
 
-    print(" Nº | Equipo | PG | PE | PP | Puntos")
-    print("-------------------------------------")
 
-    posicion = 1
-    for equipo, pg, pe, pp, pts in datos_ordenados:
-        print(f"{posicion:2} | {equipo:15} | {pg:2} | {pe:2} | {pp:2} | {pts:3}")
-        posicion += 1
